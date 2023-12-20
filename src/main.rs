8bc807e7e73fdbfac7d22d938bb0ae4298aa4ba8
Vexionix <djdarkstefan@gmail.com>
2023-12-20 19:13:50 +0200

    updated eval_expression

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 fn eval_expression(expr: &str) -> bool {
-    let expr_copy = expr.clone();
-    for part in expr_copy.split(" ") {
-        match part {
-            "+" => {}
-            "-" => {}
-            "*" => {}
-            "/" => {}
-            _ => {
-                match part.parse::<f64>() {
-                    Ok(_float_value) => { // temporary
-                    }
-                    Err(_) => return false,
+    let mut chars = expr.chars().peekable();
+    let mut number_buffer = String::new();
+
+    while let Some(&c) = chars.peek() {
+        if c.is_whitespace() {
+            chars.next();
+        } else if (c >= '0' && c <= '9') || c == '.' {
+            while let Some(&c) = chars.peek() {
+                if (c >= '0' && c <= '9') || c == '.' {
+                    number_buffer.push(c);
+                    chars.next();
+                } else {
+                    break;
                 }
             }
+            let number: f64;
+            match number_buffer.parse::<f64>() {
+                Ok(float_value) => {
+                    number = float_value;
+                }
+                Err(_) => return false,
+            }
+            number_buffer.clear();
+            println!("{}", number);
+        } else {
+            match c {
+                '+' | '-' | '*' | '/' | '^' | '(' | ')' => {}
+                _ => return false,
+            };
+            chars.next();
         }
     }
     return true;
 }
 
 fn main() {
-    let input = "1.5 + 3 - 2";
+    let input = "1.53 + 3 - 2";
     if eval_expression(input) == true {
         println!("Correct.");
     } else {
