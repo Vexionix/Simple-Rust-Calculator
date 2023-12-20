b771e19077ae848fb219ed063488a31fd5165831
Vexionix <djdarkstefan@gmail.com>
2023-12-20 18:24:54 +0200

    very basic eval

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn eval_expression(expr: &str) -> bool {
+    let expr_copy = expr.clone();
+    for part in expr_copy.split(" ") {
+        match part {
+            "+" => {}
+            "-" => {}
+            "*" => {}
+            "/" => {}
+            _ => {
+                match part.parse::<f64>() {
+                    Ok(_float_value) => { // temporary
+                    }
+                    Err(_) => return false,
+                }
+            }
+        }
+    }
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "1.5 + 3 - 2";
+    if eval_expression(input) == true {
+        println!("Correct.");
+    } else {
+        println!("Incorrect.");
+    }
 }
