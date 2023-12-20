926605989f77f869e379c24b3c830ef511f2e46c
Vexionix <djdarkstefan@gmail.com>
2023-12-20 20:03:18 +0200

    modified eval_expression to lex the expression into terms (returns a vector)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-fn eval_expression(expr: &str) -> bool {
+use core::panic;
+
+enum Term {
+    Number(f64),
+    Op(char),
+    Sin,
+    Cos,
+    Log,
+    LeftParen,
+    RightParen,
+}
+
+fn lex(expr: &str) -> Vec<Term> {
     let mut chars = expr.chars().peekable();
     let mut number_buffer = String::new();
     let mut function_buffer = String::new();
+    let mut terms = Vec::new();
 
     while let Some(&c) = chars.peek() {
         if c.is_whitespace() {
@@ -17,42 +30,51 @@ fn eval_expression(expr: &str) -> bool {
             }
             let number: f64 = match number_buffer.parse::<f64>() {
                 Ok(float_value) => float_value,
-                Err(_) => return false,
+                Err(_) => panic!("Invalid expression."),
             };
+            terms.push(Term::Number(number));
             number_buffer.clear();
-            println!("{}", number);
-        } else {
-            match c {
-                '+' | '-' | '*' | '/' | '^' | '(' | ')' => {}
-                _ => {
-                    if c.is_ascii_alphabetic() {
-                        while let Some(&c) = chars.peek() {
-                            if c.is_ascii_alphabetic() {
-                                function_buffer.push(c);
-                                chars.next();
-                            } else {
-                                break;
-                            }
-                        }
-                        match function_buffer.as_str() {
-                            "log" | "sin" | "cos" => {}
-                            _ => return false,
-                        }
-                        function_buffer.clear();
-                    }
+        } else if c.is_ascii_alphabetic() {
+            while let Some(&c) = chars.peek() {
+                if c.is_ascii_alphabetic() {
+                    function_buffer.push(c);
+                    chars.next();
+                } else {
+                    break;
                 }
-            };
+            }
+            terms.push(match function_buffer.as_str() {
+                "log" => Term::Log,
+                "sin" => Term::Sin,
+                "cos" => Term::Cos,
+                _ => panic!("Invalid expression."),
+            });
+            function_buffer.clear();
+        } else {
+            terms.push(match c {
+                '+' | '-' | '*' | '/' | '^' => Term::Op(c),
+                '(' => Term::LeftParen,
+                ')' => Term::RightParen,
+                _ => panic!("Invalid expression"),
+            });
             chars.next();
         }
     }
-    true
+    terms
 }
 
 fn main() {
     let input = "1.53 + 3 * sin(3) - 2";
-    if eval_expression(input) {
-        println!("Correct.");
-    } else {
-        println!("Incorrect.");
+    let terms = lex(input);
+    for i in terms {
+        match i {
+            Term::Number(float_value) => println!("{}", float_value),
+            Term::Op(c) => println!("{}", c),
+            Term::Sin => println!("sin"),
+            Term::Cos => println!("cos"),
+            Term::Log => println!("log"),
+            Term::LeftParen => println!("("),
+            Term::RightParen => println!(")"),
+        }
     }
 }
